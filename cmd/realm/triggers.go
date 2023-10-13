8601b00671b532857a5e37abd056311d434472bd
Eddy Babetto <eddy.babetto@secomind.com>
2023-10-13 18:32:53 +0200

    added trigger save

--- cmd/realm/triggers.go
+++ cmd/realm/triggers.go
@@ -18,9 +18,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/astarte-platform/astartectl/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // triggersCmd represents the triggers command
@@ -69,6 +71,18 @@ var triggersDeleteCmd = &cobra.Command{
 	Aliases: []string{"del"},
 }
 
+var triggersSaveCmd = &cobra.Command{
+	Use:   "save [destination-path]",
+	Short: "Save triggers to a local folder",
+	Long: `Save each trigger in a realm to a local folder. Each trigger will
+be saved in a dedicated file whose name will be in the form '<trigger_name>_v<version>.json'.
+When no destination path is set, triggers will be saved in the current working directory.
+This command does not support the --to-curl flag.`,
+	Example: `  astartectl realm-management triggers save`,
+	Args:    cobra.MaximumNArgs(1),
+	RunE:    triggersSaveF,
+}
+
 func init() {
 	RealmManagementCmd.AddCommand(triggersCmd)
 
@@ -77,6 +91,7 @@ func init() {
 		triggersShowCmd,
 		triggersInstallCmd,
 		triggersDeleteCmd,
+		triggersSaveCmd,
 	)
 }
 
@@ -176,4 +191,112 @@ func triggersDeleteF(command *cobra.Command, args []string) error {
 
 	fmt.Println("ok")
 	return nil
+}
+
+func triggersSaveF(command *cobra.Command, args []string) error {
+	if viper.GetBool("realmmanagement-to-curl") {
+		fmt.Println(`'triggers save' does not support the --to-curl option. Use 'triggers list' to get the triggers in your realm, 'triggers versions' to get their versions, and 'triggers show' to get the content of an interface.`)
+		os.Exit(1)
+	}
+
+	var targetPath string
+	var err error
+	if len(args) == 0 {
+		targetPath, _ = filepath.Abs(".")
+	} else {
+		targetPath, err = filepath.Abs(args[0])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+
+	// retrieve triggers list
+	realmTriggers, err := ListTriggers(realm)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	//ifaceNameAndVersions := map[string][]int{}
+
+	// // and the versions for each interface
+	// for _, ifaceName := range realmTriggers {
+	// 	interfaceVersions, err := interfaceVersions(ifaceName)
+	// 	if err != nil {
+	// 		fmt.Fprintln(os.Stderr, err)
+	// 		os.Exit(1)
+	// 	}
+	// 	ifaceNameAndVersions[ifaceName] = ifaceName
+	// }
+
+	for _, name := range realmTriggers {
+
+		triggerDefinition, err := getTriggerDefinition(realm, name)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		respJSON, err := json.MarshalIndent(triggerDefinition, "", "  ")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		filename := fmt.Sprintf("/%s/%s.json", targetPath, name)
+		outFile, err := os.Create(filename)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer outFile.Close()
+
+		if _, err := outFile.Write(respJSON); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	}
+	return nil
+}
+func ListTriggers(realm string) ([]string, error) {
+	listTriggersCall, err := astarteAPIClient.ListTriggers(realm)
+	if err != nil {
+		return []string{}, err
+	}
+
+	utils.MaybeCurlAndExit(listTriggersCall, astarteAPIClient)
+
+	listTriggersRes, err := listTriggersCall.Run(astarteAPIClient)
+	if err != nil {
+		return []string{}, err
+	}
+	rawlistTriggers, err := listTriggersRes.Parse()
+	if err != nil {
+		return []string{}, err
+	}
+	return rawlistTriggers.([]string), nil
+}
+func getTriggerDefinition(realm, triggerName string) (map[string]interface{}, error) {
+	getTriggerCall, err := astarteAPIClient.GetTrigger(realm, triggerName)
+	if err != nil {
+		return nil, err
+	}
+
+	// When we're here in the context of `interfaces sync`, the to-curl flag
+	// is always false (`interfaces sync` has no `--to-curl` flag)
+	// and thus the call will never exit unexpectedly
+	utils.MaybeCurlAndExit(getTriggerCall, astarteAPIClient)
+
+	getTriggerRes, err := getTriggerCall.Run(astarteAPIClient)
+	if err != nil {
+		return nil, err
+	}
+	rawTRigger, err := getTriggerRes.Parse()
+	if err != nil {
+		return nil, err
+	}
+	triggerDefinition, _ := rawTRigger.(map[string]interface{})
+	return triggerDefinition, nil
 }
